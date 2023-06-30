fe5412b130d072b94850922ab3b7849eb05ab421
woodalinka <nedalinka@gmail.com>
2023-06-30 07:25:08 -0500

    Added the button to Add Habit in the header

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
       <BrowserRouter>
-        <Header />
+        <Header onAddHabit={() => setShowModal(true)}/>
         <Routes>
           <Route
               path="/"
