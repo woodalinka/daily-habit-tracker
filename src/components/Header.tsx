835ef74fcc408ed5eefeb166ca96c3c38dd37dd7
woodalinka <nedalinka@gmail.com>
2023-06-30 06:32:34 -0500

    Installed tailwindCSS and formatted the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     return (
-        <div>
+        <nav className="bg-orange-500 p-4">
             <Link to="/" className="text-white mr-4">Habit</Link>
             <Link to="/focus" className="text-white mr-4">Focus</Link>
             <Link to="/journal" className="text-white mr-4">Journal</Link>
             <Link to="/other" className="text-white mr-4">Other</Link>
-        </div>
+        </nav>
     );
 }
 
