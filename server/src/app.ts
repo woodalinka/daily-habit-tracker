4385cf770d2c8b23a393871b2e0256bf8c8f9b01
woodalinka <nedalinka@gmail.com>
2023-08-04 08:00:50 -0500

    set up the server with typeScript

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World')
+})
+
+app.listen(5000, () => {
+    console.log("Server is up on port 5000")
+})
