27eaf4bcf93f14d3ceba38066cffd98bbca9362b
woodalinka <nedalinka@gmail.com>
2023-08-17 07:57:24 -0500

    Schema password validation added

--- server/dist/db/mongoose.js
+++ server/dist/db/mongoose.js
@@ -1,12 +1,20 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
+const mongoose_1 = require("mongoose");
 const validator_1 = __importDefault(require("validator"));
-mongoose_1.default.connect('mongodb://127.0.0.1:27017/habits-api');
-const userSchema = new mongoose_1.default.Schema({
+const userSchema = new mongoose_1.Schema({
     name: {
         type: String,
         required: true,
@@ -22,29 +30,64 @@ const userSchema = new mongoose_1.default.Schema({
                 throw new Error('Email is invalid');
             }
         }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value.length < 6) {
+                throw new Error('Too short. Minimum 6 characters');
+            }
+            if (value.toLowerCase().includes('password')) {
+                throw new Error(`Can't use the word "password" in your password`);
+            }
+        }
     }
 });
-const User = mongoose_1.default.model('User', userSchema);
-const habitSchema = new mongoose_1.default.Schema({
-    name: String,
-    occurrence: Number
+const User = (0, mongoose_1.model)('User', userSchema);
+const createUser = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, mongoose_1.connect)('mongodb://127.0.0.1:27017/habits-api');
+    const me = new User({
+        name: 'Maria    ',
+        email: "NEDALINKA@gmail.com  ",
+        password: "Password   "
+    });
+    yield me.save().then(() => {
+        console.log(me);
+    }).catch((error) => {
+        console.log("error", error);
+    });
 });
-const Habit = mongoose_1.default.model('Habit', habitSchema);
-const me = new User({
-    name: 'Alina    ',
-    email: "NEDALINKA@gmail.com  "
+createUser().catch(err => console.log(err));
+const habitSchema = new mongoose_1.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    occurrence: {
+        type: Number,
+        required: true
+    },
+    completion: {
+        type: Boolean,
+        default: false
+    }
 });
-me.save().then(() => {
-    console.log(me);
-}).catch((error) => {
-    console.log("error", error);
+//
+const Habit = (0, mongoose_1.model)('Habit', habitSchema);
+const createHabit = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, mongoose_1.connect)('mongodb://127.0.0.1:27017/habits-api');
+    const newHabit = new Habit({
+        description: "Program",
+        occurrence: 7,
+        completion: false
+    });
+    yield newHabit.save().then(() => {
+        console.log(newHabit);
+    }).catch((error) => {
+        console.log(error);
+    });
 });
-const habit = new Habit({
-    name: 'Program Everyday',
-    occurrence: "never"
-});
-// habit.save().then(() => {
-//     console.log(habit)
-// }).catch((error) => {
-//     console.log("error", error)
-// })
+// createHabit().catch(err => console.log(err))
--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -1,4 +1,4 @@
-import {Schema, model, connect, Mongoose} from "mongoose";
+import {Schema, model, connect} from "mongoose";
 import validator from "validator";
 interface User {
     name: string;
@@ -32,7 +32,17 @@ const userSchema = new Schema<User>({
     password: {
         type: String,
         required: true,
+        trim: true,
+        minlength: 7,
+        validate(value: string) {
+            if (value.length < 7) {
+                throw new Error ('Too short. Minimum 7 characters')
+            }
 
+            if (value.toLowerCase().includes('password')){
+                throw new Error (`Can't use the word "password" in your password`)
+            }
+        }
     }
 })
 
@@ -41,8 +51,9 @@ const createUser = async () => {
     await connect('mongodb://127.0.0.1:27017/habits-api')
 
     const me = new User({
-        name:'Alina    ',
-        email: "NEDALINKA@gmail.com  "
+        name:'Maria    ',
+        email: "NEDALINKA@gmail.com  ",
+        password: "Password   "
     })
 
     await me.save().then(() =>{
@@ -55,20 +66,39 @@ const createUser = async () => {
 createUser().catch(err => console.log(err))
 
 
-// const habitSchema = new mongoose.Schema({
-//     name: String,
-//     occurrence: Number
-// })
+const habitSchema = new Schema<Habit>({
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+
+    },
+    occurrence: {
+        type: Number,
+        required: true
+    },
+    completion: {
+        type: Boolean,
+        default: false
+    }
+})
 //
-// const Habit = mongoose.model('Habit', habitSchema)
+const Habit = model('Habit', habitSchema)
 
-// const habit = new Habit ({
-//     name: 'Program Everyday',
-//     occurrence: "never"
-// })
+const createHabit = async () => {
+    await connect('mongodb://127.0.0.1:27017/habits-api')
 
-// habit.save().then(() => {
-//     console.log(habit)
-// }).catch((error) => {
-//     console.log("error", error)
-// })
+    const newHabit = new Habit({
+        description: "Program",
+        occurrence: 7,
+        completion: false
+    })
+
+    await newHabit.save().then(() => {
+        console.log(newHabit)
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+// createHabit().catch(err => console.log(err))
